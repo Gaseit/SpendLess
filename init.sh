2747b4b7559d47f693cdbd0d8cdac9dae499004c
bielllabres55 <82681787+bielllabres55@users.noreply.github.com>
2022-05-23 20:02:37 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+# Intalación de los modulos para el servidor express
 cd express
 npm install
+
+# Intalación de los modulos para react
 cd ../web
 npm install
+
+# Entramos en la carpeta con el docker
 cd ../docker
+# Paramos los dockers por si se estan ejecutando
 docker-compose down
+# Cambiamos permisos de la carpeta con la base de datos para que no de problemas
 sudo chown -R 27:sudo ./sql/data
+# Iniciamos los containers
 docker-compose up -d --build
